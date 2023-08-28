1c82b7aae963d2827d4269dc6941a6969b3f6e6d
hasanabill <hasanabilna@gmail.com>
2023-08-29 00:18:50 +0600

    level one done

---
+++ Level-1/five.java
@@ -0,0 +1,5 @@
+public class five {
+    public static void main(String[] args) {
+        System.out.println("Difference= " + (45 - 32) + " Product= " + (45 * 32));
+    }
+}
---
+++ Level-1/four.java
@@ -0,0 +1,5 @@
+public class four {
+    public static void main(String[] args) {
+        System.out.println("Sum of 2,4 and 5 is " + (2 + 4 + 5));
+    }
+}
